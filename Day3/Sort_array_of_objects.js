4fd9f28ec2075109fcdae5601c66d5ef1932b75e
ApoorvaDA <apoorvaajai@gmail.com>
2016-02-12 00:50:30 -0600

    Day 3 - Challenge started
    Hacker rank Day3 challenges including displaying information from JS
    objects, sorting array of objects

---
+++ Day3/Sort_array_of_objects.js
@@ -0,0 +1,44 @@
+/*
+Task
+
+Write a JavaScript function to sort an array, library, of objects containing information about books by their title. 
+The array library is given in the template of the code. 
+You only have to fill in the relevant parts of the code to print the sorted library array by the title, in lexicographical order. 
+No input and no sample test cases, you can directly submit your solution.
+*/
+function sortLibrary() {
+    // var library is defined, use it in your code
+    // use console.log(library) to output the sorted library data
+    var arr = [];
+    for(var a in library){
+        arr.push({
+            'author' : library[a].author,
+            'title' : library[a].title,
+            'libraryID' : library[a].libraryID });
+    }    
+
+    arr.sort(function(a,b){return a.title.localeCompare(b.title)});
+    console.log(arr);
+    
+} 
+
+// tail starts here
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        libraryID: 1254
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        libraryID: 4264
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        libraryID: 3245
+    }
+];
+
+sortLibrary();
